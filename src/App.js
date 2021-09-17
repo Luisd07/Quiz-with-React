c8e45fe599fc84b8b9c0be39e2d77969e7651673
luisd07 <luimd0728@gmail.com>
2021-09-16 23:12:49 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Do some cool stuff bro
+        </p>
+        <a
+          className="App-link"
+          href="https://youtube.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go watch youtube
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
